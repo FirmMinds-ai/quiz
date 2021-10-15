f2c0e9a0768974a7f60228faa2bee6d233ca2ee9
FirmMinds-ai <87647533+FirmMinds-ai@users.noreply.github.com>
2021-10-15 11:38:34 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -12,13 +12,18 @@ st.text(" \n")
 
 
 
+
 st.write('What did you saw first?')
-option_51 = st.checkbox('Lips')
+# option_51,option_52,option_53 = True ,True ,True
+
+option_51 = st.button('A Lips')
+
 if option_51 :
     st.write("you always take things the way they are and judge them by their true value. You don’t try to change or decipher things around you or understand their hidden meaning.")
-option_52 = st.checkbox('Trees')
+
+option_52 = st.button('B Trees')
 if option_52 :
     st.write("you are an ambitious person who always looks further than others. Moreover, you are a perfectionist to the core and search for the best things in any situation.")
-option_53 = st.checkbox('Roots')
-if option_53 :
-    st.write("you are an extremely progressive person always trying to improve or change a situation. ")
+option_53 = st.button('C Roots')
+if  option_53:
+     st.write("you are an extremely progressive person always trying to improve or change a situation. ")
