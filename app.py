d719cd10a62c4b5366cdf6d8b16da12fab2ed082
FirmMinds-ai <87647533+FirmMinds-ai@users.noreply.github.com>
2021-10-15 12:13:57 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 from PIL import Image
 
 
-
+count = 0
 st.header("Question 5")
 
 img= Image.open("5 (2).jpg")
@@ -18,18 +18,19 @@ st.text(" \n")
 st.write('What did you saw first?')
 
 
-option_51 = st.button('A Lips')
+option_51 = st.checkbox('A Lips')
 
 if option_51 :
     st.write("you always take things the way they are and judge them by their true value. You don’t try to change or decipher things around you or understand their hidden meaning.")
-
-option_52 = st.button('B Trees')
+    count += 1
+option_52 = st.checkbox('B Trees')
 if option_52 :
     st.write("you are an ambitious person who always looks further than others. Moreover, you are a perfectionist to the core and search for the best things in any situation.")
-option_53 = st.button("C Roots")
+    count += 1
+option_53 = st.checkbox("C Roots")
 if  option_53:
      st.write("you are an extremely progressive person always trying to improve or change a situation. ")
-
+     count += 1
 
 
 
@@ -47,12 +48,20 @@ st.text(" \n")
 
 st.write('What did you saw first?')
 
-option_61 = st.button("A Crocodile")
+option_61 = st.checkbox("A Crocodile")
 
 if option_61 :
     st.write(" you can be a little careless and inattentive to details by nature. Moreover, you are a practical person who doesn’t like to take risks")
-
-option_62 = st.button('B Boat')
+    count += 1
+option_62 = st.checkbox('B Boat')
 
 if option_62 :
     st.write("you like nuances and slightly visible details. You prefer to stand out of the crowd as well as be a creative and unique person. Anyway, try to picture the world as a holistic thing, and don’t waste your time on the meaningless details")
+    count += 1
+
+st.text(" \n")
+st.text(" \n")
+
+
+st.header("Total Answers given")
+st.write(str(count))
